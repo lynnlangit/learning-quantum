ff8cc7712cc4281bada3ceb270f339c8e11e9f2b
Lynn Langit <lynnlangit@live.com>
2021-04-13 19:01:48 -0600

    Create 2-1.js

---
+++ book-code/2-1.js
@@ -0,0 +1,13 @@
+// Programming Quantum Computers
+//   by Eric Johnston, Nic Harrigan and Mercedes Gimeno-Segovia
+//   O'Reilly Media
+
+// To run this online, go to http://oreilly-qc.github.io?p=2-1
+
+// This sample generates a single random bit.
+
+qc.reset(1);         // allocate one qubit
+qc.write(0);         // write the value zero
+qc.had();            // place it into superposition of 0 and 1
+var result = qc.read();  // read the result as a digital bit
+qc.print(result + ' is the output\n');  
