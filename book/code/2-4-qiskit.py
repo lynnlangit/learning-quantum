5a09318c17b1cbc28cc7fd69dc94db2ba456797b
Lynn Langit <lynnlangit@live.com>
2021-04-26 21:07:01 -0600

    Update 2-4-qiskit.py

--- book/code/2-4-qiskit.py
+++ book/code/2-4-qiskit.py
@@ -20,15 +20,15 @@ circuit.h(qreg_alice[0])                      # HAD Alice's qubit
 circuit.measure(qreg_alice[0], creg_aval[0])  # READ Alice's qubit value into Aval
 
 circuit.reset(qreg_alice[0])
-circuit.x(qreg_alice[0]).c_if(creg_aval, 1)  # ??? the circuit and check if Alice's qubit is 1
-circuit.h(qreg_alice[0]).c_if(creg_ahad, 1)  # HAD the circuit and check if Alice's qubit is 1
+circuit.x(qreg_alice[0]).c_if(creg_aval, 1)  # NOT Alice's qubit if Aval is 1
+circuit.h(qreg_alice[0]).c_if(creg_ahad, 1)  # HAD Alice's qubit if Ahad is 1
 circuit.swap(qreg_alice[0], qreg_fiber[0])   # SWAP Alice's qubit value into Fiber qubit
-circuit.barrier(qreg_alice[0], qreg_fiber[0], qreg_bob[0]) # ???
+circuit.barrier(qreg_alice[0], qreg_fiber[0], qreg_bob[0]) # PREVENT transformations accross this source line
 circuit.h(qreg_fiber[0])                     # HAD Fiber qubit
 circuit.measure(qreg_fiber[0], creg_fval[0]) # READ Fiber qubit value into Fval
 
 circuit.reset(qreg_fiber[0])
-circuit.x(qreg_fiber[0]).c_if(creg_fval, 1)  # ??? the circuit and check if Fiber qubit is 1
+circuit.x(qreg_fiber[0]).c_if(creg_fval, 1)  # NOT Fiber qubit if Fval is 1
 circuit.h(qreg_fiber[0])                     # HAD Fiber qubit
 circuit.barrier(qreg_alice[0], qreg_fiber[0], qreg_bob[0]) # PREVENT transformations accross this source line
 
@@ -36,7 +36,7 @@ circuit.reset(qreg_bob[0])
 circuit.h(qreg_bob[0])                       # HAD Bob's qubit
 circuit.measure(qreg_bob[0], creg_bhad[0])   # READ Bob's qubit value into Bhad
 circuit.swap(qreg_fiber[0], qreg_bob[0])     # SWAP Fiber qubit value into Bob's qubit
-circuit.h(qreg_bob[0]).c_if(creg_bhad, 1)    # HAD the circuit and check if Bob's qubit is 1
+circuit.h(qreg_bob[0]).c_if(creg_bhad, 1)    # HAD Bob's qubit if Bhad is 1
 circuit.measure(qreg_bob[0], creg_bval[0])   # READ Bob's qubit value into Bval
 
 editor = CircuitComposer(circuit=circuit)
