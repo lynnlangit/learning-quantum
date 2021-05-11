c22a2213e7a9f93f0a6b266e58fa08a6d5522928
Lynn Langit <lynnlangit@live.com>
2021-05-10 21:58:12 -0500

    Update 3-6-lynn.js

--- book/code/ch3/3-6-lynn.js
+++ book/code/ch3/3-6-lynn.js
@@ -4,17 +4,16 @@ var b = qint.new(1, 'b');
 qc.write(0);
 
 a.had();
-// now prob of a is 50%
+//qc.print(a.read(),b.read());  // inject to examine
+
+// Prob of a is 50%
 b.had();
 b.phase(45);        // COMMENT out this phase to understand Line 10
 b.had();            // HAD after PHASE --> prob of b is 15%
 b.cnot(a);
 
-// Now, you can read *either*
-// qubit and get 50% prob.
-// If the result is 0, then
-// the prob of the *remaining*
-// qubit is 15%, else it's 85%
+// Now, you can read *either* qubit and get 50% prob.
+// If the result is 0, then the prob of the *remaining* qubit is 15%, else it's 85%
 var a_result = a.read();
 var b_result = b.read();
 qc.print(a_result + ' ');
