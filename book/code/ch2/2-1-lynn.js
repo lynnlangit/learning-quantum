ef09a4f69ae50e3c52855c51e1a353b31dba647b
Lynn Langit <lynnlangit@live.com>
2021-05-13 08:34:45 -0500

    Update 2-1-lynn.js

--- book/code/ch2/2-1-lynn.js
+++ book/code/ch2/2-1-lynn.js
@@ -1,13 +1,17 @@
-// Programming Quantum Computers
-//   by Eric Johnston, Nic Harrigan and Mercedes Gimeno-Segovia
-//   O'Reilly Media
-
 // This sample generates a single random bit.
 
-qc.print(`${new Date()}\n\n`);
-
+// create a register with one qubit
 qc.reset(1);
+
+// set 0 as the qubit's value
 qc.write(0);
+
+// set the qubit into superposition
 qc.had();
+
+// collapse the qubit and read the value
 var result = qc.read();
+
+// print the result value
 qc.print('result: ' + result + '\n');
+qc.print(`${new Date()}\n\n`);
