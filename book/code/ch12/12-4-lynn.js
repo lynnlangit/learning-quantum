d7b9171f0e0c24b1ed9c6f1f648dbf5e8c258f6e
Lynn Langit <lynnlangit@live.com>
2021-07-12 10:52:49 -0500

    Create 12-4-lynn.js

---
+++ book/code/ch12/12-4-lynn.js
@@ -0,0 +1,35 @@
+var spike_position = 8;
+var data_range = 16;
+
+var num_spikes = estimate_num_spikes(spike_position, data_range);
+
+qc.print('Estimated number of spikes: ' + num_spikes + '\n');
+
+function estimate_num_spikes(spike, range)
+{
+    if (spike < range / 2)
+        spike = range - spike;
+    var best_error = 1.0;
+    var e0 = 0;
+    var e1 = 0;
+    var e2 = 0;
+    var actual = spike / range;
+    var candidates = []
+    for (denom = 1.0; denom < spike; ++denom)
+    {
+        var numerator = Math.round(denom * actual);
+        var estimated = numerator / denom;
+        var error = Math.abs(estimated - actual);
+        e0 = e1;
+        e1 = e2;
+        e2 = error;
+        // Look for a local minimum which beats our current best error
+        if (e1 <= best_error && e1 < e0 && e1 < e2)
+        {
+            var repeat_period = denom - 1;
+            candidates.push(denom - 1);
+            best_error = e1;
+        }
+    }
+    return candidates;
+}
